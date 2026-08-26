ffe62344d3ee6de74feac68c429c18dd3e8b9e8d
Federico Ramos <federico.g.ramos@gmail.com>
2026-08-26 00:27:37 -0300

    falla

--- clean_comments_inout.py
+++ clean_comments_inout.py
@@ -82,3 +82,51 @@ def traducir_a_latex(md_texto):
 	md_texto = re.sub(pattern_tab, table_sub, md_texto, flags=re.DOTALL)
 	md_texto = re.sub(r'\\begin{table}.*?\\end{table}', placeholder, md_texto)
 	md_texto = md_texto.replace('_', '\\_') 
+	md_texto = re.sub(r'<b>(.*?)</b>', r'\\textbf{\1}', md_texto, flags=re.DOTALL)
+	md_texto = re.sub(r'\*\*(.*?)\*\*', r'\\textbf{\1}', md_texto)
+	md_texto = re.sub(r'`(.*?)`', r'\\texttt{\1}', md_texto)
+	md_texto = re.sub(r'\*(.*?)\*', r'\\textit{\1}', md_texto) 
+	for _ in range(3):
+		md_texto = re.sub(r'<blocknote>(.*?)</blocknote>', r'\\begin{quote}\n\\color{gray}\n\1\n\\end{quote}', md_texto, flags=re.DOTALL)
+		md_texto = re.sub(r'<color\s+name=["\'](.*?)["\']>(.*?)</color>', r'{\\color{\1}\2}', md_texto, flags=re.DOTALL)
+		def font_sub(match):
+			tipo = match.group(1).strip().lower()
+			contenido = match.group(2)
+			fuentes = {'tt': 'texttt', 'sf': 'textsf', 'rm': 'textrm', 'sc': 'textsc'}
+			comando = fuentes.get(tipo, 'texttt')
+			return f"\\{comando}{{{contenido}}}"
+		md_texto = re.sub(r'<font\s+name=["\'](.*?)["\']>(.*?)</font>', font_sub, md_texto, flags=re.DOTALL)
+	def anchor_sub(match):
+		url_real = match.group(1).strip()
+		texto_visible = match.group(2)
+		latex_href = f"\\href{{{url_real}}}{{{texto_visible}}}"
+		class FakeMatch:
+			def __init__(self, val): self.val = val
+			def group(self, num): return self.val
+		return placeholder(FakeMatch(latex_href))
+	md_texto = re.sub(r'<a\s+href=["\'](.*?)["\']\s*>(.*?)</a>', anchor_sub, md_texto, flags=re.DOTALL)
+	md_texto = re.sub(r'^####\s+<center>\s*(.*?)\s*</center>$', lambda m: proteger_titulo(fr'\leavevmode{{\centering \normalsize {m.group(1)}\par}}\vspace*{{{ESP_CENTER_H4}}}'), md_texto, flags=re.M)
+	md_texto = re.sub(r'^###\s+<center>\s*(.*?)\s*</center>$', lambda m: proteger_titulo(fr'\leavevmode{{\centering \large {m.group(1)}\par}}\vspace*{{{ESP_CENTER_H3}}}'), md_texto, flags=re.M)
+	md_texto = re.sub(r'^##\s+<center>\s*(.*?)\s*</center>$', lambda m: proteger_titulo(fr'\leavevmode{{\centering \Large {m.group(1)}\par}}\vspace*{{{ESP_CENTER_H2}}}'), md_texto, flags=re.M)
+	md_texto = re.sub(r'^#\s+<center>\s*(.*?)\s*</center>$', lambda m: proteger_titulo(fr'\leavevmode{{\centering \LARGE {m.group(1)}\par}}\vspace*{{{ESP_CENTER_H1}}}'), md_texto, flags=re.M)
+	md_texto = re.sub(r'^####\s+(.+)$', lambda m: proteger_titulo(fr'\vspace*{{{ESP_PRE_H4}}}\noindent{{\normalsize {m.group(1)}\par}}\vspace*{{{ESP_LEFT_H4}}}'), md_texto, flags=re.M)
+	md_texto = re.sub(r'^###\s+(.+)$',  lambda m: proteger_titulo(fr'\vspace*{{{ESP_PRE_H3}}}\noindent{{\large {m.group(1)}\par}}\vspace*{{{ESP_LEFT_H3}}}'), md_texto, flags=re.M)
+	md_texto = re.sub(r'^##\s+(.+)$',   lambda m: proteger_titulo(fr'\vspace*{{{ESP_PRE_H2}}}\noindent{{\Large {m.group(1)}\par}}\vspace*{{{ESP_LEFT_H2}}}'), md_texto, flags=re.M)
+	md_texto = re.sub(r'^#\s+(.+)$',    lambda m: proteger_titulo(fr'\vspace*{{{ESP_PRE_H1}}}\noindent{{\LARGE {m.group(1)}\par}}\vspace*{{{ESP_LEFT_H1}}}'), md_texto, flags=re.M)
+	md_texto = re.sub(r'<center>(.*?)</center>', fr'{{\\centering \1\\par}}\vspace*{{{ESP_CENTER_GENERIC}}}', md_texto, flags=re.DOTALL)
+	pattern_merge = r'</problem>\s*<problem>'
+	while re.search(pattern_merge, md_texto, flags=re.DOTALL):
+		md_texto = re.sub(pattern_merge, '\n', md_texto, flags=re.DOTALL)
+	md_texto = re.sub(r'<problem>(.*?)</problem>', r'\\begin{problem}\n\1\n\\end{problem}', md_texto, flags=re.DOTALL)
+	md_texto = md_texto.replace('&nbsp;', '~')
+	md_texto = md_texto.replace('&ensp;', '\\quad ')   
+	md_texto = md_texto.replace('&emsp;', '\\qquad ')  
+	md_texto = re.sub(r'^\s*</?br\s*/?>\s*$', fr'~\\par\\vspace*{{{ESP_BREAK}}}', md_texto, flags=re.M | re.IGNORECASE)
+	md_texto = re.sub(r'</?br\s*/?>', r'\\\\ ', md_texto, flags=re.IGNORECASE)
+	md_texto = re.sub(r'^---\s*$', r'\\hrule\n\\vspace{0.4cm}', md_texto, flags=re.M)
+	md_texto = re.sub(r'^\s*[\*\-]\s+(.+)$', r'\\item \1', md_texto, flags=re.M)
+	md_texto = re.sub(r'((?:\\item .+(?:\n|$))+)', r'\\begin{itemize}\n\1\\end{itemize}', md_texto)
+	for _ in range(2):
+		for key, original_content in math_blocks.items():
+			md_texto = md_texto.replace(key, original_content)
+	return md_texto
